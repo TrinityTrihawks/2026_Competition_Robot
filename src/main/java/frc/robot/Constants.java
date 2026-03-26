42f8e5d1e59c447fb740ff317e83f92674bb4420
Nathan Hartwell <nathan.hartwell@publicsq.com>
2026-03-26 08:13:04 -0500

    Add field centric auto align

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -52,6 +52,8 @@ public class Constants {
         public static final String KEY_VISION_AVG_TAG_DIST = "Vision/AvgTagDist";
         public static final String KEY_ANGLE_ERROR_LIMELIGHT = "Angle Error for Limelight";
         public static final String KEY_TAGS = "tags";
+
+        public static final String KEY_ALIGN_MODE_CHOOSER = "Auto Aim Mode";
     }
 
     public static final class VisionConstants {
--- src/main/java/frc/robot/commands/AlignShooting.java
+++ src/main/java/frc/robot/commands/AlignShooting.java
@@ -4,7 +4,9 @@ import java.util.Set;
 
 import com.pathplanner.lib.auto.AutoBuilder;
 
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Constants;
@@ -15,14 +17,38 @@ import frc.robot.subsystems.CommandSwerveDrivetrain;
 
 public class AlignShooting extends SequentialCommandGroup {
 
-    public AlignShooting(CommandSwerveDrivetrain drivetrain) {
-        addCommands(
-                Commands.defer(() ->
-                                AutoBuilder.pathfindToPose(NavUtil.FindShootTarget(drivetrain.getState().Pose.getTranslation(), () -> SmartDashboard.getNumber(Constants.SmartDashboardConstants.KEY_TARGET_SHOOTING_DIST, Constants.SmartDashboardConstants.DEFAULT_TARGET_SHOOTING_DIST)), DynamicPathDemo.DEFAULT_CONSTRAINTS),
-                        Set.of(drivetrain)),
-                new AlignToHub(drivetrain),
-                new AlignDistance(drivetrain));
+    public enum AlignMode {
+        ROBOT_CENTRIC,
+        FIELD_CENTRIC
     }
 
+    private static final SendableChooser<AlignMode> alignModeChooser = new SendableChooser<>();
 
+    static {
+        alignModeChooser.setDefaultOption("Robot-Centric", AlignMode.ROBOT_CENTRIC);
+        alignModeChooser.addOption("Field-Centric", AlignMode.FIELD_CENTRIC);
+        SmartDashboard.putData(Constants.SmartDashboardConstants.KEY_ALIGN_MODE_CHOOSER, alignModeChooser);
+    }
+
+    public AlignShooting(CommandSwerveDrivetrain drivetrain) {
+        addCommands(
+                Commands.defer(() -> {
+                    AlignMode mode = alignModeChooser.getSelected();
+                    Command pathfind = AutoBuilder.pathfindToPose(
+                            NavUtil.FindShootTarget(
+                                    drivetrain.getState().Pose.getTranslation(),
+                                    () -> SmartDashboard.getNumber(
+                                            Constants.SmartDashboardConstants.KEY_TARGET_SHOOTING_DIST,
+                                            Constants.SmartDashboardConstants.DEFAULT_TARGET_SHOOTING_DIST)),
+                            DynamicPathDemo.DEFAULT_CONSTRAINTS);
+
+                    if (mode == AlignMode.FIELD_CENTRIC) {
+                        return pathfind.andThen(new FieldCentricAlign(drivetrain));
+                    } else {
+                        return pathfind
+                                .andThen(new AlignToHub(drivetrain))
+                                .andThen(new AlignDistance(drivetrain));
+                    }
+                }, Set.of(drivetrain)));
+    }
 }
