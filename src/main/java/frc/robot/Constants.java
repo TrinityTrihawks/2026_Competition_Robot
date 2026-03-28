4da3be7f20c134ed7735014de6a07b40296096cc
YNeil Forbes <forbeneil5@gmail.com>
2026-03-28 14:11:07 -0500

    changed default for shooting dist

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -41,7 +41,7 @@ public class Constants {
         public static final String KEY_LIMELIGHT_CHOOSER = "Limelight On/Off";
 
         public static final String KEY_TARGET_SHOOTING_DIST = "Target Shooting Dist";
-        public static final double DEFAULT_TARGET_SHOOTING_DIST = 2;
+        public static final double DEFAULT_TARGET_SHOOTING_DIST = 2.5;
 
         // Telemetry keys
         public static final String KEY_BATTERY_VOLTAGE = "Battery Voltage";
